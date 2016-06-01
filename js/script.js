aacfa69abf4ed6bbf565875a62fcf097201719a4
Callan McNulty and Matt Wilkin <matt@>
2016-06-01 09:37:32 -0700

    added user interface.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Business (or bach-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -26,7 +28,14 @@ var ftoc = function(temp){
   return multiply(subtract(temp,32),divide(5,9));
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = bmi(number1, number2);
-alert(result);
+//User interface (or front-end) logic
+
+$(document).ready(function() {
+  $("form.add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
